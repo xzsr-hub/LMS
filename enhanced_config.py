b538f1fb14570f6186194280b276ad365b6fdfb2
Tonglin Wang <2845143697@qq.com>
2025-08-10 02:22:58 -0700

    Update enhanced_config.py

--- enhanced_config.py
+++ enhanced_config.py
@@ -1,6 +1,6 @@
 HOST = 'localhost'
 USER = 'root'
-PASSWORD = 'Xzsr.4143.91'
+PASSWORD = '替换'
 DATABASE = 'library_management'
 PORT = 3306
 CHARSET = 'utf8mb4'
@@ -8,4 +8,5 @@ CHARSET = 'utf8mb4'
 # 业务配置
 DEFAULT_BORROW_DAYS = 30  # 默认借阅天数
 FINE_PER_DAY = 0.5  # 每天罚金
+
 MAX_BORROW_BOOKS = 5  # 默认最大借书数量 
